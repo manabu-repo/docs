4ec98ec338ce108699f258517cd03644f27cffe2
Akashi <akashi_sai@163.com>
2022-02-20 17:31:36 +0800

    :pencil: docs: commits

---
+++ test/test.string.js
@@ -0,0 +1,7 @@
+const { at, chartAt, concat } = require('../js/string');
+
+// const testAt = chartAt(1);
+// console.log('at', testAt);
+
+const testConcat = concat('akashi');
+console.log('concat', testConcat);
