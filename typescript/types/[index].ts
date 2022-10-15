e8f37ea84f160946038aa0237fa0d9d739b75d98
Akashi <akashi_sai@163.com>
2022-10-15 11:51:10 +0800

    :art: chore: update [index].ts

--- typescript/types/[index].ts
+++ typescript/types/[index].ts
@@ -5,15 +5,15 @@
 // infer: 占位符关键字，可以结合...一起使用
 // ['length']: 获取长度
 // as: 断言语句 => as const 添加只读属性
-// Partial<Type>
-// Required<Type>
-// Readonly<Type>
-// Record<Type>
-// Pick<Type, Keys>
-// Omit<Type, Keys>
-// Exclude<UnionType, ExcludedMembers>
-// Extract<Type, Union>
-// NonNullable<Type>
-// Parameters<Type>
-// ReturnType<Type>
-// InstanceType<Type>
+// Partial<Type> 将类型 Type 的所有属性标记为可选属性
+// Required<Type> 将类型 Type 的所有属性标记为必选属性
+// Readonly<Type> 只读属性，不可修改
+// Pick<Type, Keys> 从 Type 中过滤出 Keys
+// Record<K, T> 标记对象的 key value 类型
+// Omit<Type, Keys> 移除 T 中的 U 属性
+// Exclude<UnionType, ExcludedMembers> 移除 T 中的 U 属性
+// Extract<Type, Union>  Exclude 的反操作，取 T，U两者的交集属性
+// NonNullable<Type> 排除类型 T 的 null | undefined 属性
+// Parameters<Type> 获取一个函数的所有参数类型
+// ReturnType<Type> 获取函数类型 T 的返回类型
+// InstanceType<Type> 获取一个类的返回类型
